7c1a7b7887c80ea2f878ecd56eb0d448061266bd
Freek van de Ven <freek@iconica.nl>
2022-08-17 09:53:53 +0200

    feat: add movie detail navigation

--- lib/src/ui/screens/detail.dart
+++ lib/src/ui/screens/detail.dart
@@ -1,11 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:movie_viewing_app/src/models/models.dart';
+import 'package:movie_viewing_app/src/providers.dart';
 import 'package:movie_viewing_app/src/ui/screens/base.dart';
 
-class MovieDetailScreen extends StatelessWidget {
+class MovieDetailScreen extends ConsumerStatefulWidget {
   const MovieDetailScreen({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<MovieDetailScreen> createState() => _MovieDetailScreenState();
+}
+
+class _MovieDetailScreenState extends ConsumerState<MovieDetailScreen> {
+  late Movie movie;
+  late MovieUserSettings movieSettings;
+
+  @override
+  void initState() {
+    movieSettings = ref
+        .read(movieSettingsProvider)
+        .firstWhere((element) => element.selected);
+    movie = ref
+        .read(movieCatalogueProvider)
+        .firstWhere((element) => element.title == movieSettings.title);
+    super.initState();
+  }
+
+  Future<void> _onPageExit() async {
+    await ref
+        .read(movieSettingsProvider.notifier)
+        .updateMovieUserSettings(movieSettings.copyWith(selected: false));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const BaseScreen(child: Center(child: Text('Movie Detail Screen')));
+    return WillPopScope(
+      onWillPop: () async {
+        await _onPageExit();
+        return true;
+      },
+      child: BaseScreen(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // backbutton
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () async {
+                    var navigator = Navigator.of(context);
+                    await _onPageExit();
+                    navigator.pop();
+                  },
+                ),
+                // likebutton
+                IconButton(
+                  icon: const Icon(Icons.favorite),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Text(movie.title, style: Theme.of(context).textTheme.headline3),
+          ],
+        ),
+      ),
+    );
   }
 }
