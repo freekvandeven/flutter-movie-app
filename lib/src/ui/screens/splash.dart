8be5d9c23579e817cff977b2d20e4e0f9077c217
Freek van de Ven <freek@iconica.nl>
2022-08-18 09:15:37 +0200

    feat: add quick_actions

--- lib/src/ui/screens/splash.dart
+++ lib/src/ui/screens/splash.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_viewing_app/src/movieroute.dart';
 import 'package:movie_viewing_app/src/providers.dart';
 import 'package:movie_viewing_app/src/ui/screens/base.dart';
+import 'package:quick_actions/quick_actions.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,8 +12,12 @@ class SplashScreen extends ConsumerStatefulWidget {
 }
 
 class _SplashScreenState extends ConsumerState<SplashScreen> {
+  String shortcut = 'no action set';
+
   @override
   void initState() {
+    super.initState();
+
     ref.read(movieCatalogueProvider.notifier).fetchMovies();
     ref.read(actorProvider.notifier).fetchActors();
     ref.read(movieSettingsProvider.notifier).fetchMovieUserSettings();
@@ -29,7 +34,36 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
         }
       }
     });
-    super.initState();
+
+    var quickActions = const QuickActions()
+      ..initialize((String shortcutType) {
+        setState(() {
+          shortcut = shortcutType;
+        });
+      });
+
+    quickActions.setShortcutItems(<ShortcutItem>[
+      // NOTE: This first action icon will only work on iOS.
+      // In a real world project keep the same file name for both platforms.
+      const ShortcutItem(
+        type: 'action_one',
+        localizedTitle: 'Action one',
+        icon: 'AppIcon',
+      ),
+      // NOTE: This second action icon will only work on Android.
+      // In a real world project keep the same file name for both platforms.
+      const ShortcutItem(
+        type: 'action_two',
+        localizedTitle: 'Action two',
+        icon: 'ic_launcher',
+      ),
+    ]).then((void _) {
+      setState(() {
+        if (shortcut == 'no action set') {
+          shortcut = 'actions ready';
+        }
+      });
+    });
   }
 
   @override
